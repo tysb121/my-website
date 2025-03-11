2aae1015bf4773e7802b4a422d8a4d1c73294b3d
yangjinlong <318635011@qq.com>
2025-03-11 23:37:04 +0800

    增加动态视频背景

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -5,8 +5,28 @@
         <loader></loader>
       </div>
     </transition> -->
+    <!-- 加载动画 -->
     <Loading :isloading="isloading" />
-    <div class="vapp-fullscreen-background" :class="{ isMobile: isMobileDevice }"></div>
+    <!-- 背景图片 -->
+    <div
+      v-if="bgType === 1"
+      class="vapp-fullscreen-background"
+      :class="{ isMobile: isMobileDevice }"
+    ></div>
+    <!-- 动态视频背景 -->
+    <video
+      v-if="bgType === 2"
+      autoplay
+      loop
+      muted
+      class="video-bg"
+      :class="{ isMobile: isMobileDevice }"
+      id="bg-video"
+      ref="VdPlayer"
+    >
+      <source :src="videosrc" type="video/mp4" />
+    </video>
+    <!-- 内容 -->
     <transition name="slide-up">
       <div class="home-container" v-if="containerFlag">
         <el-row align="middle" justify="center">
@@ -55,18 +75,20 @@ import ApplicationView from './HomeComponents/applicationView.vue'
 // import Loader from '@/components/Loader.vue'
 import Loading from '@/components/Loading.vue'
 import imgUrl from '@/assets/3.jpg'
-import { reactive, toRefs, onBeforeMount, computed, onMounted } from 'vue'
+import { reactive, toRefs, onBeforeMount, computed, onMounted, ref } from 'vue'
 import { Icon } from '@vicons/utils'
 import { Link } from '@vicons/fa' // 注意使用正确的类别
-
+const VdPlayer = ref<HTMLVideoElement>()
 const state = reactive({
   circleUrl: imgUrl,
   lastSelectedIndex: -1,
   isloading: true,
   bgImgSrc: '',
   containerFlag: false,
+  videosrc: '',
+  bgType: 1, //1为静态图片，2为动态视频
 })
-const { circleUrl, lastSelectedIndex, isloading, containerFlag } = toRefs(state)
+const { circleUrl, lastSelectedIndex, isloading, containerFlag, videosrc, bgType } = toRefs(state)
 
 // 计算属性来判断是否为移动端
 const isMobileDevice = computed(() => {
@@ -81,8 +103,8 @@ function setBackgroundImg() {
     'static/安逸舒适/image.png',
     'static/海洋女孩/image.png',
     'static/书房夜晚/image.png',
-    // 'dynamic/尼尔：机械纪元 团队/Nier-Automata-Team.webm',
-    // 'dynamic/向往航天的女孩/Toy-Aeroplane.webm',
+    'dynamic/尼尔：机械纪元 团队/Nier-Automata-Team.webm',
+    'dynamic/向往航天的女孩/Toy-Aeroplane.webm',
   ]
   for (let i = 1; i <= 10; i++) {
     //添加另外一个文件夹的图片
@@ -90,9 +112,9 @@ function setBackgroundImg() {
   }
 
   const imgFileMobile = [
-    // 'dynamic-mobile/幻觉镇-gaako_illust/Hallucination_town.mp4',
-    // 'dynamic-mobile/chuva/chuva.mp4',
-    // 'dynamic-mobile/Doodle-小猫女仆降临/d12.mp4',
+    'dynamic-mobile/幻觉镇-gaako_illust/Hallucination_town.mp4',
+    'dynamic-mobile/chuva/chuva.mp4',
+    'dynamic-mobile/Doodle-小猫女仆降临/d12.mp4',
   ]
   for (let i = 1; i <= 4; i++) {
     imgFileMobile.push(`static-mobile/000${i}/image.png`)
@@ -114,13 +136,20 @@ function setBackgroundImg() {
   } else {
     imgSrc = imgFilePC[newIndex]
   }
-  state.bgImgSrc = `/img/wallpaper/${imgSrc}`
 
-  root.style.setProperty('--background-image-url', `url('${state.bgImgSrc}')`)
+  // 判断是否为动态图片
+  if (imgSrc.includes('dynamic')) {
+    state.bgType = 2
+    state.videosrc = `/img/wallpaper/${imgSrc}`
+  } else {
+    state.bgType = 1
+    state.bgImgSrc = `/img/wallpaper/${imgSrc}`
+    root.style.setProperty('--background-image-url', `url('${state.bgImgSrc}')`)
+  }
 }
 
 function loadImage() {
-  const imageUrls = [state.bgImgSrc]
+  const imageUrls = state.bgImgSrc ? [state.bgImgSrc] : []
   return new Promise((resolve, reject) => {
     const imagePromises = imageUrls.map((url) => {
       return new Promise((resolve, reject) => {
@@ -140,25 +169,27 @@ function loadImage() {
 
     // 等待所有图片加载完成或超时
     Promise.race([Promise.all(imagePromises), timeoutPromise]).then(() => {
-      // if(imageurl){
-      const img = new Image()
-      img.src = state.bgImgSrc
-      // resolve() 函数通将一个 Promise 对象从未完成状态转变为已完成状态
-      img.onload = () => {
-        resolve(true)
+      if (state.bgType === 1) {
+        const img = new Image()
+        img.src = state.bgImgSrc
+        // resolve() 函数通将一个 Promise 对象从未完成状态转变为已完成状态
+        img.onload = () => {
+          resolve(true)
+        }
+        img.onerror = (err) => {
+          reject(err)
+        }
+      } else {
+        const video = VdPlayer.value
+        if (!video) return
+        video.onloadedmetadata = () => {
+          setTimeout(() => {}, 200)
+          resolve(true)
+        }
+        video.onerror = (err) => {
+          resolve(true)
+        }
       }
-      img.onerror = (err) => {
-        reject(err)
-      }
-      // }else{
-      //   const video = this.$refs.VdPlayer;
-      //   video.onloadedmetadata = () => {
-      //     setTimeout(() => {
-      //     }, "200");
-      //     resolve();
-      //   };
-      //   video.onerror = (err) => {resolve();};
-      // }
     })
   })
 }
@@ -236,6 +267,27 @@ onMounted(() => {
   align-items: center;
   gap: 10px;
 }
+
+/* 调整视频元素的样式，使其全屏作为背景 */
+.video-bg {
+  position: fixed;
+  height: 96.6%;
+  width: 99%;
+  top: 1.7%;
+  left: 0.5%;
+  object-fit: cover;
+  margin: auto;
+  background-clip: padding-box;
+  filter: brightness(85%);
+  border-radius: 16px;
+  &.isMobile {
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    border-radius: unset;
+}
+}
 .vapp-fullscreen-background {
   position: fixed;
   height: 96.6%;
@@ -248,7 +300,7 @@ onMounted(() => {
     height: 100%;
     top: 0;
     left: 0;
-    &::before{
+    &::before {
       border-radius: unset;
     }
   }
